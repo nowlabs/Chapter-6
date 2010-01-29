f8fb26bfb26f0438aad6c335745d08c4e28f71ea
Thomas Eapen <thomas@nowlabs.net>
2010-01-28 21:40:01 -0500

    first commit

---
+++ Engine.h
@@ -0,0 +1,16 @@
+//
+//  Engine.h
+//  Chapter 6
+//
+//  Created by Thomas Eapen on 09-12-28.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Engine : NSObject {
+
+}
+
+@end
---
+++ Tire.h
@@ -0,0 +1,7 @@
+
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Tire : NSObject
+@end //Tire Interface
